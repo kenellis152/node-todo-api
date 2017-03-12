affee6f14aeeb7ba94d8020c8ece58c8fd05a0a4
Ken Ellis <kenellis152@gmail.com>
2017-03-12 13:00:41 -0400

    Test the DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,39 +4,16 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-var id = '58c41f2eb6510fb43335b7f5';
+//Todo.remove({})
+// can't pass in empty document to remove everything, have to pass empty ObjectID
 
-if (!ObjectID.isValid(id)) {
-  console.log('ID not valid');
-}
+Todo.remove({}).then( (result) => {
+  console.log(result);
+});
 
-// Todo.find({
-//   _id: id
-// }).then( (todos) => {
-//   console.log('Todos', todos);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then( (todo) => {
-//   console.log('Todo', todo);
-// });
+// Todo.findOneAndRemove    //both of these return the doc
+// Todo.findByIdAndRemove
 
-// Todo.findById(id).then( (todo) => {
-//   if (!todo) {
-//     return console.log('Id not found');
-//   }
-//   console.log('Todo by ID', todo);
-// }).catch((e) => console.log(e));
+Todo.findByIdAndRemove('asdf').then( (doc) => {
 
-// User.findByID
-// case 1: it works, but no user
-// case 2: user found, print it to screen
-// case 3: handle errors
-
-User.findById(id).then( (user) => {
-  if(!user) {
-    return console.log('User not found');
-  }
-  console.log('User by ID: ', user);
-}).catch( (e) => console.log(e));
+});
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -92,7 +92,7 @@ describe('Get /todos/:id', () => {
      .expect(404)
      .end(done);
   });
- 
+
   //pass /todos/123
   it('should return a 404 for non-object ids', (done) => {
     request(app)
@@ -102,3 +102,40 @@ describe('Get /todos/:id', () => {
   });
 
 });
+
+describe('DELETE /todos/:id', () => {
+  it ('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect( (res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end( (err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then( (todos) =>{
+          expect(todos).toNotExist();
+          done();
+        }).catch( (e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if objectid is invalid', (done) => {
+    request(app)
+     .delete(`/todos/123`)
+     .expect(404)
+     .end(done);
+  });
+})
